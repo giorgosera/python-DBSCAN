156bac2c00abf8c972c832e27d8a921ad5e775b4
George Eracleous <basketballcy@gmail.com>
2012-02-08 11:59:38 +0000

    Added dbscan class

---
+++ dbscan.py
@@ -0,0 +1,17 @@
+'''
+Created on 8 Feb 2012
+
+@author: George Eracleous ge108@ic.ac.uk
+'''
+
+class DBSCANClustering(object):
+    '''
+    A Python implementation of the DBSCAN clustering algorithm.
+    '''
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pass
+        
